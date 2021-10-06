4772eff9722f881811491997c3a67e947bb18e21
Krapi Rastogi <75130582+krapirastogi@users.noreply.github.com>
2021-10-07 02:32:09 +0530

    Add files via upload
    concept of arithmetic ,logical ,relational and assignment operators explained in brief

---
+++ JavaScript-Learn/operators_krapirastogi.js
@@ -0,0 +1,24 @@
+// AUTHOR: Krapi Rastogi
+// JavaScript Concept:Operators
+// GITHUB: https://github.com/krapirastogi
+
+//An operators are  mathematical symbols that produces a result based on two values (or variables)
+//Arithmetic Operators
+console.log(6+9)//Addition of two numbers
+console.log("Hello"+" " +"world")//Conacatenation Of two strings
+console.log(9-6)//Subtraction of two integers
+console.log(9*3)//Multiplication of two integers
+console.log(9/3)//Division of two integers
+
+//Assignment Operator
+let x=6//Assigning a value of 6 to the variable x
+let y=5
+//Logical Operators
+//'===' checks whether two expressions are equal or not and returns a boolean value (true or false)
+// '!=='/'!' logically opposite of '==='(true or false)
+if(x===6 && y===5)
+console.log("yay!")
+//will print yay! only if both conditions are true
+if(x===6 || y===4)
+console.log("yay!")
+//will print yay! if either of the conditions are true
