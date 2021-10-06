7c6aa1609a023e189c885150cce2cbeefd28c9ba
Ajinkya <ajinkya.deshpande0000@gmail.com>
2021-10-07 00:40:42 +0530

    add for loop

---
+++ JavaScript-Learn/forLoop_ajinkya2000.js
@@ -0,0 +1,8 @@
+// AUTHOR: Ajinkya Deshpande
+// JavaScript Concept: For Loop
+// GITHUB: https://github.com/ajinkya2000
+
+// Printing numbers from 1 to 10
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
