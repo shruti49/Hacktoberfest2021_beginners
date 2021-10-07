559caba2efe9a0714338aded3ffea6e1e4d08718
yysrll <yusril.uzumaki@gmail.com>
2021-10-08 01:27:18 +0800

    add array sum

---
+++ JavaScript-Learn/sum_array_yysrll.js
@@ -0,0 +1,18 @@
+// AUTHOR: Yusril
+// JavaScript Concept: Algorithm
+// GITHUB: https://github.com/yysrll
+
+//Add your JavaScript concept below
+
+function arraySum(arr) {
+    var sum = 0;
+
+    for (var i = 0; i < ar.length; i++) {
+      sum += arr[i];
+    }
+
+    return sum;
+  }
+  
+//Run on browser to see the output
+console.log(simpleArraySum([1, 3, 5, 7]))
