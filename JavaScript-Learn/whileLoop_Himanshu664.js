833342a5705ba3e687dce9ef032d6e3a39f46936
Himanshu <himanshumalviya100@gmail.com>
2021-10-07 10:06:09 +0530

    while loop added

---
+++ JavaScript-Learn/whileLoop_Himanshu664.js
@@ -0,0 +1,40 @@
+// AUTHOR: Himanshu Malviya
+// JavaScript Concept: While Loop
+// GITHUB: https://github.com/Himanshu664
+
+
+
+
+// while Statement
+
+// The while statement executes the statements until the condition is not false. The
+// syntax for 'while' loop is 
+
+// Syntax
+while (condition) {
+    // code block to be executed
+  }
+
+
+  Ex:
+  while (i < 10) {
+    text += "The number is " + i;
+    i++;
+  }
+//   output:
+//   The number is 0
+//   The number is 1
+//   The number is 2
+//   The number is 3
+//   The number is 4
+//   The number is 5
+//   The number is 6
+//   The number is 7
+//   The number is 8
+//   The number is 9
+
+
+
+
+// First, the condition is evaluated and if it is true, then the statements are executed and
+// again the condition is tested. The execution stops when condition returns false.
