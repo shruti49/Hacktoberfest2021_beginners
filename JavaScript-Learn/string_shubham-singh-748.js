79bd4746848de4403daa4dd7b3a38a1b5449f1cc
Shubham Rajput <shubhamsingh74892@gmail.com>
2021-10-07 09:21:08 +0530

    Added a javascript concept

---
+++ JavaScript-Learn/string_shubham-singh-748.js
@@ -0,0 +1,8 @@
+//AUTHOR: Shubham Singh
+//JavaScript Concept: String
+//GITHUB: https://github.com/shubham-singh-748
+
+//Added my javascript concept below
+var msg; 
+msg = "Hello JavaScript!";
+alert(msg);
