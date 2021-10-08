4dc2e2eb4c6b603e95926e04bfdce2e79be4fff1
Ankita Patil <73190796+Ankita-Nandkumar-Patil@users.noreply.github.com>
2021-10-08 15:01:47 +0530

    Create JS_array_Ankita-Nandkumar-Patil.js
    this code in JavaScript will create an array, then remove some elements from it. And will replace them with the elements passed.

---
+++ JS_array_Ankita-Nandkumar-Patil.js
@@ -0,0 +1,11 @@
+// AUTHOR: Ankita Nandkumar Patil
+// JavaScript Concept: array is JS, remove and replace array elements
+// GITHUB: https://github.com/Ankita-Nandkumar-Patil
+
+
+let arr = ["I", "Am", "Contributing", "to", "this", "repo"];
+
+// remove 3 first elements and replace them with another
+arr.splice(2, 3, "requesting", "PR");
+
+alert( arr ) 
