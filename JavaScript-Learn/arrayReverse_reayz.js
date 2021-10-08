e6e47b17a180af3511d41bc9554b3c9b71a5e7ed
reayz <reayz77@yahoo.com>
2021-10-08 12:41:53 +0600

    adding a reverse function

---
+++ JavaScript-Learn/arrayReverse_reayz.js
@@ -0,0 +1,14 @@
+// AUTHOR: Reajul Haque Reayz
+// JavaScript Concept: reverse array
+// GITHUB: https://github.com/Reayz
+
+// reverse array in javascript
+
+var array = [1,2,3,4,5];
+
+console.log(array.reverse());
+
+console.log(array);
+
+// output
+// [ 5, 4, 3, 2, 1 ]
