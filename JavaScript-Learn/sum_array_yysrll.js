26175027038e736e6bf0ec0aad21829c7e99502b
yysrll <yusril.uzumaki@gmail.com>
2021-10-08 13:09:54 +0800

    fix: add consept explain

--- JavaScript-Learn/sum_array_yysrll.js
+++ JavaScript-Learn/sum_array_yysrll.js
@@ -3,7 +3,7 @@
 // GITHUB: https://github.com/yysrll
 
 //Add your JavaScript concept below
-
+//function
 function arraySum(arr) {
     var sum = 0;
 
@@ -13,6 +13,15 @@ function arraySum(arr) {
 
     return sum;
   }
+
+Ex:
+var arr_sample = [1, 2, 3, 4];
+arraySum(arr_sample);
+
+//output = 10
+//the function add each number in the array and the array item must be integer, so we get output 10
+// 1 + 2 + 3 + 4 = 10
   
-//Run on browser to see the output
+//Run to see the output
 console.log(simpleArraySum([1, 3, 5, 7]))
+// expect output 1 + 3 + 5 + 7 = 16
