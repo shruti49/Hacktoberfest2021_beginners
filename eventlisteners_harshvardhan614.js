b079adfb5c753ffb2b338d91f2d5df48bcbc4420
HARSHVARDHAN RATHORE <76932639+harshvardhan614@users.noreply.github.com>
2021-10-11 18:17:43 +0530

    Update eventlisteners_harshvardhan614.js

--- eventlisteners_harshvardhan614.js
+++ eventlisteners_harshvardhan614.js
@@ -2,21 +2,22 @@
 // JavaScript Concept: Eventlisteners in Javascript
 // GITHUB: https://github.com/harshvardhan614
 
+// Eventlistener in Javascript
 //An Eventlistener is a method in JavaScript that attaches an event to to an element.
 //The listener is programed to start an event when the input or signaling the event handler.
 
 //Syntax :-
-element.addEventListener(event, function, useCapture);
+//element.addEventListener(event, function, useCapture);
 
-some EventListeners are :-
-onload //when the page loads
-onclick //when a user clicks something
-onmouseover //when a user mouses over something
-onmouseup //when a user mouses down from something
-onmousedown //when a user mouses up from something
-onmouseout //when a user mouses out something
-onfocus //when a user puts the cursor in a form field
-onblur //When a user leaves a form field
+//some EventListeners are :-
+//onload //when the page loads
+//onclick //when a user clicks something
+//onmouseover //when a user mouses over something
+//onmouseup //when a user mouses down from something
+//onmousedown //when a user mouses up from something
+//onmouseout //when a user mouses out something
+//onfocus //when a user puts the cursor in a form field
+//onblur //When a user leaves a form field
 
 // On clicking button this  function works
  <button id="btn" onclick="clicked()">Click me</button>
