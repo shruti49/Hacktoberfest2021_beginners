de3874408ffc77eeecf3ca461c0f2ff18cb68d52
Adarsh Kushwaha <57234525+Adarsh-kushwaha@users.noreply.github.com>
2021-10-12 22:46:42 +0530

    Debouncing concept added

---
+++ JavaScript-Concepts/Debouncing_adarshkushwaha.js
@@ -0,0 +1,22 @@
+// AUTHOR: Adarsh Kushwaha
+// JavaScript Concept: Debouncing in javascript
+// GITHUB: https://github.com/Adarsh-kushwaha
+
+//Debouncing in JavaScript is a practice used to improve browser performance
+
+//func: It is the func function that we want to execute after the debounce time.
+
+//wait: The time after the last received action that the debounce function can wait until executing func.
+
+const debounce = (func, wait) => {  
+  let timeout;  
+  return function mainFunction(...args) {  
+    const later = () => { 
+      //Timeout: The timeout function is the value that is used to indicate a running debounce.
+      clearTimeout(timeout);  
+      func(...args);  
+    };  
+    clearTimeout(timeout);  
+    timeout = setTimeout(delay, wait);  
+  };  
+};  
