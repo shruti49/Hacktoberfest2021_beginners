8cdb916d20cab088ac26b303a592260f58ce4eae
Shruti Shastri <shrutishastri49@gmail.com>
2021-10-14 10:59:10 +0530

    added prototype

--- JavaScript-Concepts/prototype_GauravPatil8778.js
+++ JavaScript-Concepts/prototype_GauravPatil8778.js
@@ -4,20 +4,23 @@
 
 
 
-<script>  
 function Employee(firstName,lastName)  
 {  
   this.firstName=firstName;  
   this.lastName=lastName;  
 }  
-  
+    
 Employee.prototype.fullName=function()  
-  {  
-    return this.firstName+" "+this.lastName;  
-  }  
+{  
+  return this.firstName+" "+this.lastName;  
+}  
+
+Employee.prototype.company="Javatpoint"  
   
 var employee1=new Employee("Martin","Roy");  
 var employee2=new Employee("Duke", "William");  
 document.writeln(employee1.fullName()+"<br>");  
 document.writeln(employee2.fullName());  
-</script>  
+document.writeln(employee1.firstName+" "+employee1.lastName+" "+employee1.company+"<br>");  
+document.writeln(employee2.firstName+" "+employee2.lastName+" "+employee2.company);  
+
