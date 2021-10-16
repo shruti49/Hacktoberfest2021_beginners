4b31c3a3eb886d80896ed6aadb98a1724af5d167
tokyo-arch <83420072+tokyo-arch@users.noreply.github.com>
2021-10-17 02:00:59 +0530

    JavaScript static methods

---
+++ JavaScript-Concepts/JavaScriptStaticMethod.js
@@ -0,0 +1,22 @@
+//The JavaScript provides static methods that belong to the class instead of an instance of that class. So, an instance is not required to call the static method.
+//These methods are called directly on the class itself.
+
+<script>  
+class Test  
+{  
+  static display1()  
+  {  
+    return "static method is invoked"  
+  }  
+  static display2()  
+  {  
+    return "static method is invoked again"  
+  }  
+}  
+document.writeln(Test.display1()+"<br>");  
+document.writeln(Test.display2());  
+</script>  
+
+// OUTPUT
+//static method is invoked
+//static method is invoked again
