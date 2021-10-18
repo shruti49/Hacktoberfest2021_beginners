07801eefdf37b9a858d51c5f121c91c2377130c8
Arunavo Biswas <65866038+DuesExMachina@users.noreply.github.com>
2021-10-18 23:58:05 +0530

    Sample script 4
    PR 4

---
+++ JavaScript-Concepts/sample_JS4_Arunavo.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Variables</h2>
+
+<p>A variable declared without a value will have the value undefined.</p>
+
+<p id="demo"></p>
+
+<script>
+var carName;
+document.getElementById("demo").innerHTML = carName;
+</script>
+
+</body>
+</html>
