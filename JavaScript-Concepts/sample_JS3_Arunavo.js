ec4ae100afbc465b58e2b584898953c6cd37cd32
Arunavo Biswas <65866038+DuesExMachina@users.noreply.github.com>
2021-10-18 23:55:48 +0530

    Sample script 3
    PR no 3

---
+++ JavaScript-Concepts/sample_JS3_Arunavo.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Variables</h2>
+
+<p>The result of adding "John" + " " + "Doe":</p>
+
+<p id="demo"></p>
+
+<script>
+var x = "John" + " " + "Doe";
+document.getElementById("demo").innerHTML = x;
+</script>
+
+</body>
+</html>
