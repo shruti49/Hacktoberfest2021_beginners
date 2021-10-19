3e838db95ba0e7fa9cc09e8d1757d09130ee8d9a
riishika <72249656+riishika@users.noreply.github.com>
2021-10-19 18:24:50 +0530

    StringMethods_riishika.js
    The indexOf() method returns the position of the first occurrence of a specified text

---
+++ JavaScript-Concepts/StringMethods_riishika.js
@@ -0,0 +1,21 @@
+// AUTHOR: Rishika
+// JavaScript Concept: String Methods
+// GITHUB: https://github.com/riishika
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript String Methods</h2>
+
+<p>The indexOf() method returns the position of the first occurrence of a specified text:</p>
+
+<p id="demo"></p>
+
+<script>
+let str = "Please locate where 'locate' occurs!";
+document.getElementById("demo").innerHTML = str.indexOf("locate");
+</script>
+
+</body>
+</html>
