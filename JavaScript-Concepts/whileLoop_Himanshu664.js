64231b6bbdd4c269745719632fcb3f0302f780b1
Nikhil Singh <singhnikhiljsr@gmail.com>
2021-10-22 16:43:30 +0530

    Added Do While Loop

--- JavaScript-Concepts/whileLoop_Himanshu664.js
+++ JavaScript-Concepts/whileLoop_Himanshu664.js
@@ -17,6 +17,8 @@ while (condition) {
 
 
   Ex:
+
+  i=0;
   while (i < 10) {
     text += "The number is " + i;
     i++;
@@ -38,3 +40,39 @@ while (condition) {
 
 // First, the condition is evaluated and if it is true, then the statements are executed and
 // again the condition is tested. The execution stops when condition returns false.
+
+
+
+// AUTHOR: Nikhil Singh
+// JavaScript Concept: Do While Loop
+// GITHUB: https://github.com/singhnikhiljsr/
+
+
+// The do while loop is a variant of the while loop. This loop will execute the code block once, before checking if the condition is true, then it will repeat the loop as long as the condition is true.
+// Syntax //
+
+do {
+  // code block to be executed
+}
+while (condition);
+
+// Example //
+
+i=0;
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
+
+// Output
+// The number is 0
+// The number is 1
+// The number is 2
+// The number is 3
+// The number is 4
+// The number is 5
+// The number is 6
+// The number is 7
+// The number is 8
+// The number is 9
