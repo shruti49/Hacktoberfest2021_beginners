cbb5dedfa309739e9e8963f879bbdc4a3b32b1c6
Chinmay <76864749+Chinmayk2002@users.noreply.github.com>
2021-10-22 14:54:52 +0530

    added recursion function
    added recursion concept

---
+++ JavaScript-Concepts/Recursion_Chinmayk2002.js
@@ -0,0 +1,16 @@
+// AUTHOR: Chinmay Kamble
+// JavaScript Concept: Recursion function
+// GITHUB: https://github.com/Chinmayk2002
+
+
+
+function recurse() {
+    if(condition) {
+        recurse();
+    }
+    else {
+        // stop calling recurse()
+    }
+}
+
+recurse();
