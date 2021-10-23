98c87015e10bdad52efc672c09253960a5533d1b
tokyo-arch <83420072+tokyo-arch@users.noreply.github.com>
2021-10-23 12:56:50 +0530

    Exception handling using try catch in javascript

---
+++ JavaScript-Concepts/TryCatch.js
@@ -0,0 +1,11 @@
+try{  
+var a=2;  
+if(a==2)  
+document.write("ok");  
+}  
+catch(Error){  
+document.write("Error found"+e.message);  
+}  
+finally{  
+document.write("Value of a is 2 ");  
+}  
