fe4b7fe8577a004ae947113c0290ad41a726e951
Krittiya <krittiyaclark@gmail.com>
2021-10-28 23:53:34 -0400

    Adds JavaScript concept

---
+++ JavaScript-Concepts/combineLettersInOrder.js
@@ -0,0 +1,18 @@
+// AUTHOR: Krittiya Clark
+// JavaScript Concept: for loop
+// GITHUB: https://github.com/krittiyaclark
+
+// Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+// E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+function combineLettersInOrder(strOne, strTwo, strThree) {
+	let combinedStr = ''
+	for (let i = 0; i < strOne.length; i++) {
+		combinedStr += strOne[i] + strTwo[i] + strThree[i]
+	}
+	return combinedStr
+}
+
+console.log(combineLettersInOrder('aa', 'bb', 'cc'), 'abcabc')
+console.log(combineLettersInOrder('Sea', 'urn', 'pms'), 'Supermans')
